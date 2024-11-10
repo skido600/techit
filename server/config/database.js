3b217accb70b4e3980b2ba2b1a1d39bd744bc4ce
Chuks <63956312+skido600@users.noreply.github.com>
2024-11-10 02:42:00 +0100

    techit

--- server/config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  dialect: 'sqlite',
-  storage: './server/data/database.sqlite',
-};
--- server/routes/transactions.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const transactionController = require('../controllers/transactionController');
-
-const router = new express.Router();
-
-router.get('/snipping', transactionController.snipping);
-router.get('/front', transactionController.front);
-
-module.exports = router;
